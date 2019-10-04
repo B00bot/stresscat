4f2f09f7afd95d067d24c7bf302cdf569e0f347a
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-04 20:06:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-import config
 import telebot
 from telebot import types
-import logging
-import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, RegexHandler
+import requests  
+import datetime
+import requests
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+url = "https://api.telegram.org/bot<845739116:AAHTjdzhhe526OLCj1uycnJG6jg2H3NXxfk>/"
+class BotHandler:
+
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+
+    def get_updates(self, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
+
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
+
+    def get_last_update(self):
+        get_result = self.get_updates()
+
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
+
+        return last_update
 
 @bot.message_handler(content_types=["text"])
 def get_text_messages(message):
@@ -19,23 +45,14 @@ def get_text_messages(message):
 	elif message.text == "/proof":
 		bot.send_photo(message.from_user.id, open('tsmock.jpg', 'rb'));
 	else:
-def main():
-    updater = Updater(token = '845739116:AAHTjdzhhe526OLCj1uycnJG6jg2H3NXxfk')
+		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+def main():  
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+           update_id += 1
+    sleep(1)       
 
-    dispatcher = updater.dispatcher
-
-    conv_handler = ConversationHandler(
-        entry_points = [CommandHandler('start', start)],
-
-        fallbacks=[CommandHandler('cancel', cancel)]
-    )
-
-    dispatcher.add_handler(conv_handler)
-
-    dispatcher.add_error_handler(error)
-    
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__':  
+    main()		
