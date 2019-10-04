32d62f41e432710339b177e60e6a1e0d1093a815
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-04 20:43:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def get_text_messages(message):
 		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 def main():
 	updater = Updater(token='862281743:AAEvvJc2tldFHZWXqC540GKAxvSPxpyvqPc')
-	updater.bot.polling()
+	updater.polling()
 	updater.idle()
 if __name__ == '__main__':
     main()
