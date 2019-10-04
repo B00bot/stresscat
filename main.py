e4984ae2d0969b631e2c98df5bdfc00f37476e77
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-04 17:42:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_text_messages(message):
 	else:
 		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 def main():
-    updater = Updater(token=config.token)
+    updater = Updater(token=862281743:AAEvvJc2tldFHZWXqC540GKAxvSPxpyvqPc)
 
     dispatcher = updater.dispatcher
     
