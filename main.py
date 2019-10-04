8f9c684c4f4ee201fc792dd5f606ad42a26c9a6c
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-04 20:21:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,10 @@
+# -*- coding: utf-8 -*-
+import config
 import telebot
 from telebot import types
-import requests  
-import datetime
-import requests
+from telegram.ext import Updater
 
-url = "https://api.telegram.org/bot<845739116:AAHTjdzhhe526OLCj1uycnJG6jg2H3NXxfk>/"
-class BotHandler:
-
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
-
-    def get_updates(self, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
-
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
-
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
-
-        return last_update
+bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def get_text_messages(message):
@@ -46,13 +18,9 @@ def get_text_messages(message):
 		bot.send_photo(message.from_user.id, open('tsmock.jpg', 'rb'));
 	else:
 		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-def main():  
-    update_id = last_update(get_updates_json(url))['update_id']
-    while True:
-        if update_id == last_update(get_updates_json(url))['update_id']:
-           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
-           update_id += 1
-    sleep(1)       
-
-if __name__ == '__main__':  
-    main()		
+def main():
+	updater = Updater(token='862281743:AAEvvJc2tldFHZWXqC540GKAxvSPxpyvqPc')
+	updater.start_polling()
+	updater.idle()
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
