25530f286c8da9bc2f03aef85694922e22ef298c
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-04 21:19:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ def get_text_messages(message):
 		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 		
 if __name__ == '__main__':
-while True:
-    try:
-        bot.polling(none_stop=True)
+    while True:
+        try:
+            bot.polling(none_stop=True)
 
-    except Exception as e:
-      print(e) 
-      time.sleep(15)
+        except Exception as e:
+            print(e) 
+            time.sleep(15)
