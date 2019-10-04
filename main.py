e533e76d281df84b15986b9a4a97cdc1f880aba3
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-04 20:53:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import requests
 import config
 import telebot
 from telebot import types
-from telegram.ext import Updater
 
 bot = telebot.TeleBot(config.token)
 
@@ -19,9 +18,6 @@ def get_text_messages(message):
 		bot.send_photo(message.from_user.id, open('tsmock.jpg', 'rb'));
 	else:
 		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-def main():
-	updater = Updater(token='862281743:AAEvvJc2tldFHZWXqC540GKAxvSPxpyvqPc')
-	updater.start_bot.polling()
-	updater.idle()
+		
 if __name__ == '__main__':
     bot.infinity_polling(True)
