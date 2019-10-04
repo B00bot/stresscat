a42b7e1549bcecdb2cf7a591c99781f659b3903b
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-04 14:47:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,12 +8,12 @@ bot = telebot.TeleBot(config.token)
 @bot.message_handler(content_types=["text"])
 def get_text_messages(message):
 	if message.text == "Привет":
-		bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+		bot.send_message(message.from_user.id, "Привет, если хочешь узнать тайну, отправь /secret. Если нужны доказательства - отправь /tsmock")
 	elif message.text == "/help":
 		bot.send_message(message.from_user.id, "Напиши привет")
-	elif message.text == "/secret_word":
+	elif message.text == "/secret":
 		bot.send_message(message.from_user.id, "Мой создатель любит Лапу")
-	elif message.text == "/hochu_tsmock":
+	elif message.text == "/proof":
 		bot.send_photo(message.from_user.id, open('tsmock.jpg', 'rb'));
 	else:
 		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
