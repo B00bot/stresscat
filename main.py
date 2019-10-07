10ad383ac5db02ff17ba7e198b3c225c5c23c89d
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-07 18:48:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,25 +9,24 @@ import os
 TOKEN = "862281743:AAEvvJc2tldFHZWXqC540GKAxvSPxpyvqPc"
 PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN)
+
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(content_types=["text"])
 def get_text_messages(message):
-	if message.text == "Привет":
-		bot.send_message(message.from_user.id, "Привет, если хочешь узнать тайну, отправь /secret. Если нужны доказательства - отправь /proof")
-	elif message.text == "/help":
-		bot.send_message(message.from_user.id, "Напиши привет")
-	elif message.text == "/secret":
-		bot.send_message(message.from_user.id, "Мой создатель любит Лапу")
-	elif message.text == "/proof":
-		bot.send_photo(message.from_user.id, open('tsmock.jpg', 'rb'));
-	else:
-		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-get_text_messages		
+    if message.text == "Привет":
+	bot.send_message(message.from_user.id, "Привет, если хочешь узнать тайну, отправь /secret. Если нужны доказательства - отправь /proof")
+    elif message.text == "/help":
+	bot.send_message(message.from_user.id, "Напиши привет")
+    elif message.text == "/secret":
+	bot.send_message(message.from_user.id, "Мой создатель любит Лапу")
+    elif message.text == "/proof":
+	bot.send_photo(message.from_user.id, open('tsmock.jpg', 'rb'));
+    else:
+	bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")	
 # add handlers
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
+if __name__ == '__main__':
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
 updater.bot.set_webhook("https://stresscatbot.herokuapp.com/" + TOKEN)
 updater.idle()
 
