87c4a16fd4abb5e893404d170ab87fb326b722df
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-07 19:06:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,20 +15,18 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(content_types=["text"])
 
 def get_text_messages(message):
-    if message.text == "Привет":
-	bot.send_message(message.from_user.id, "Привет, если хочешь узнать тайну, отправь /secret. Если нужны доказательства - отправь /proof")
-    elif message.text == "/help"
-bot.send_message(message.from_user.id, "Напиши привет")
-    elif message.text == "/secret":
-	bot.send_message(message.from_user.id, "Мой создатель любит Лапу")
-    elif message.text == "/proof":
-	bot.send_photo(message.from_user.id, open('tsmock.jpg', 'rb'));
-    else:
-	bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")	
+	if message.text == "Привет":
+		bot.send_message(message.from_user.id, "Привет, если хочешь узнать тайну, отправь /secret. Если нужны доказательства - отправь /proof")
+	elif message.text == "/help"
+		bot.send_message(message.from_user.id, "Напиши привет")
+	elif message.text == "/secret":
+		bot.send_message(message.from_user.id, "Мой создатель любит Лапу")
+	elif message.text == "/proof":
+		bot.send_photo(message.from_user.id, open('tsmock.jpg', 'rb'));
+	else:
+		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")	
 # add handlers
 if __name__ == '__main__'
 	updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
 	updater.bot.set_webhook("https://stresscatbot.herokuapp.com/" + TOKEN)
 	updater.idle()
-
-
