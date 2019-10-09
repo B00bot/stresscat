93077b6bafe33a1fabb71308e7ccd36f972246f7
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-09 16:35:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,25 +12,25 @@ PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(config.token)
 dispatcher = updater.dispatcher
 bot = telebot.TeleBot(config.token)
-def start():
+def start(message.text, bot.send_message):
     if message.text == "/start":
         bot.send_message(message.from_user.id, "Напиши привет")
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-def hi():
+def hi(message.text, bot.send_message):
     if message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, если хочешь узнать тайну, отправь /secret. Если нужны доказательства - отправь /proof")	
 hi_handler = CommandHandler('привет', hi)
 dispatcher.add_handler(hi_handler)
 
-def secret():
+def secret(message.text, bot.send_message):
     if message.text == "/secret":
         bot.send_message(message.from_user.id, "Мой создатель любит Лапу")
 secret_handler = CommandHandler('/secret', secret)
 dispatcher.add_handler(secret_handler)
 
-def proof():
+def proof(message.text, bot.send_message):
     if message.text == "/proof":
         bot.send_photo(message.from_user.id, open('tsmock.jpg', 'rb'));
 proof_handler = CommandHandler('/proof', proof)
