87bc5fe361f493624041b904d624bc5212335264
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-12 22:51:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,40 +3,36 @@ import requests
 import config
 import telebot
 from telebot import types
-from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 import os
 import logging
 import random
-
+updater=Updater
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(config.token)
-dispatcher = updater.dispatcher
 bot = telebot.TeleBot(config.token)
 
 
 @bot.message_handler(commands=["start"])
-def startpg(bot, update):
+def startpg(message):
     startmenu=types.ReplyKeyboardMarkup(True, False)
     startmenu.row('Секрет', 'Доказательство')
     startmenu.row('Грустно')
     startmenu.row('Нипанятнаа')
-    bot.send_message(update.message.chat.id, 'Привет. Если хочешь узнать секрет, нажми секрет. Если нужно доказательство, нажми Доказательство. Если грустно, нажми Грустно. Если нужна помошь - нажми Нипанятнаа', reply_markup=startmenu)
+    bot.send_message(message.chat.id, 'Привет. Если хочешь узнать секрет, нажми секрет. Если нужно доказательство, нажми Доказательство. Если грустно, нажми Грустно. Если нужна помошь - нажми Нипанятнаа', reply_markup=startmenu)
 
-def secret(bot, update):
-    incoming=update.message.text
+@@bot.message_handler(content_types=["text"])
+def secret(message):
+    incoming=message.text
     if incoming=='Секрет':
-        bot.sendMessage(chat_id=update.message.chat_id, text="Мой создатель любит Лапу")
-secret_handler = MessageHandler(Filters.text, secret)
-dispatcher.add_handler(secret_handler)
+        bot.sendMessage(chat_id=message.chat_id, text="Мой создатель любит Лапу")
 
-def help(bot, update):
-    incoming=update.message.text
+@bot.message_handler(content_types=["text"])
+def help(message):
+    incoming=message.text
     if incoming=='Нипанятнаа':
-        bot.sendMessage(chat_id=update.message.chat_id, text="Если хочешь узнать тайну, отправь /secret Если нужны доказательства - отправь /proof Если грустно - отправь /grustno")	
-help_handler = MessageHandler(Filters.text, help)
-dispatcher.add_handler(help_handler)
+        bot.sendMessage(chat_id=message.chat_id, text="Если хочешь узнать тайну, отправь /secret Если нужны доказательства - отправь /proof Если грустно - отправь /grustno")	
 
 #stickers for random
 stick1="CAADAgADCwADlp-MDpuVH3sws_a7FgQ"
@@ -45,7 +41,8 @@ stick3="CAADBAADfQADzjkIDSgZQLclD7jiFgQ"
 stick4="CAADBAADRAADzjkIDbv4-ULKD6hiFgQ"
 stick5="CAADAgAD0gIAArnzlwt4AXAE0tVijhYE"
 
-def proof(bot, update):
+@bot.message_handler(content_types=["text"])
+def proof(message):
     randomstick=random.randint(1,5)
     if randomstick==1:
         pic=stick1
@@ -57,20 +54,17 @@ def proof(bot, update):
         pic=stick4
     elif randomstick==5:
         pic=stick5
-    incoming=update.message.text
+    incoming=message.text
     if incoming=='Доказательство':
-        bot.sendSticker(chat_id=update.message.chat_id, sticker=pic)
-proof_handler = MessageHandler(Filters.text, proof)
-dispatcher.add_handler(proof_handler)
+        bot.sendSticker(chat_id=message.chat_id, sticker=pic)
 
-def grustno(bot, update):
-    incoming=update.message.text
+@bot.message_handler(content_types=["text"])
+def grustno(message):
+    incoming=message.text
     if incoming=='Грустно':
         pic=open('s1200.jpeg', 'rb')
-        bot.send_photo(chat_id=update.message.chat_id, photo=pic);
-        bot.sendMessage(chat_id=update.message.chat_id, text="Ни грустииии")
-grustno_handler = MessageHandler(Filters.text, grustno)
-dispatcher.add_handler(grustno_handler)
+        bot.send_photo(chat_id=message.chat_id, photo=pic)
+        bot.sendMessage(chat_id=message.chat_id, text="Ни грустииии")
 
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
