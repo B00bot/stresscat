083ab53427f51ccdc9b2847239e99ceaa19b376c
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-12 21:05:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def startpg(bot, update):
     startmenu.row('Грустно')
     startmenu.row('Нипанятнаа')
     bot.send_message(update.message.chat.id, 'Привет. Есзи хочешь узнать секрет, нажми секрет. Если нужно доказательство, нажми Доказательство. Если грустно, нажми Грустно. Если нужна помошь - нажми Нипанятнаа', reply_markup=startmenu)
-startpg_handler=CommandHandler('start', start)
+startpg_handler=CommandHandler('start', startpg)
 dispatcher.add_handler(startpg_handler)
 
 def secret(bot, update):
