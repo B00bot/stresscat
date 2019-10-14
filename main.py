17dde92f109bb0fb35e6970f1c2896e4cce6c499
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-14 15:31:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -68,8 +68,8 @@ def grustno(message):
         bot.send_photo(chat_id=message.chat_id, photo=pic)
         bot.sendMessage(chat_id=message.chat_id, text="Ни грустииии")
 
-updater.start_webhook(listen="0.0.0.0",
+updater.bot.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=config.token)
-updater.set_webhook("https://stresscatbot.herokuapp.com/" + config.token)
+updater.bot.set_webhook("https://stresscatbot.herokuapp.com/" + config.token)
 updater.idle()
