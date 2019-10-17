52bf5605cfa0e4d0d9b3d4043b6d76ae3e844634
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-17 13:51:08 +0300

    Update main.py
    Returning to working condition

--- main.py
+++ main.py
@@ -3,17 +3,30 @@ import requests
 import config
 import telebot
 from telebot import types
-from telegram.ext import CommandHandler, MessageHandler, Filters, Updater, Dispatcher
+from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters
 import os
 import logging
 import random
-updater=Updater(config.token)
-dispatcher=Dispatcher(bot, update)
-TOKEN=config.token
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 PORT = int(os.environ.get('PORT', '8443'))
+updater = Updater(config.token)
+dispatcher = updater.dispatcher
 bot = telebot.TeleBot(config.token)
+def start(bot, update):
+        bot.sendMessage(chat_id=update.message.chat_id, text="Привет, если хочешь узнать тайну, отправь /secret Если нужны доказательства - отправь /proof Если грустно - отправь /grustno")	
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+def secret(bot, update):
+        bot.sendMessage(chat_id=update.message.chat_id, text="Мой создатель любит Лапу")
+secret_handler = CommandHandler('secret', secret)
+dispatcher.add_handler(secret_handler)
+
+def help(bot, update):
+        bot.sendMessage(chat_id=update.message.chat_id, text="Если хочешь узнать тайну, отправь /secret Если нужны доказательства - отправь /proof Если грустно - отправь /grustno")	
+help_handler = CommandHandler('help', help)
+dispatcher.add_handler(help_handler)
 
 #stickers for random
 stick1="CAADAgADCwADlp-MDpuVH3sws_a7FgQ"
@@ -22,34 +35,7 @@ stick3="CAADBAADfQADzjkIDSgZQLclD7jiFgQ"
 stick4="CAADBAADRAADzjkIDbv4-ULKD6hiFgQ"
 stick5="CAADAgAD0gIAArnzlwt4AXAE0tVijhYE"
 
-
-startmenu=types.ReplyKeyboardMarkup(True, False)
-startmenu.row('Секрет', 'Доказательство')
-startmenu.row('Грустно')
-startmenu.row('Нипанятнаа')
-updater.dispatcher.add_handler(message_handler(bot, update)
-
-
-@bot.message_handler(content_types=["text"])
-def secret(update.message):
-    incoming=message.text
-    if incoming=='/start':
-        bot.send_message(chat_id=update.message.chat_id, 'Привет. Если хочешь узнать секрет, нажми секрет. Если нужно доказательство, нажми Доказательство. Если грустно, нажми Грустно. Если нужна помошь - нажми Нипанятнаа', reply_markup=startmenu)
-
-@bot.message_handler(content_types=["text"])
-def secret(update.message):
-    incoming=message.text
-    if incoming=='Секрет':
-        bot.sendMessage(chat_id=update.message.chat_id, text="Мой создатель любит Лапу")
-
-@bot.message_handler(content_types=["text"])
-def help(update.message):
-    incoming=message.text
-    if incoming=='Нипанятнаа':
-        bot.sendMessage(chat_id=update.message.chat_id, text="Если хочешь узнать тайну, отправь /secret Если нужны доказательства - отправь /proof Если грустно - отправь /grustno")	
-
-@bot.message_handler(content_types=["text"])
-def proof(update.message):
+def proof(bot, update):
     randomstick=random.randint(1,5)
     if randomstick==1:
         pic=stick1
@@ -61,20 +47,18 @@ def proof(update.message):
         pic=stick4
     elif randomstick==5:
         pic=stick5
-    incoming=message.text
-    if incoming=='Доказательство':
-        bot.sendSticker(chat_id=update.message.chat_id, sticker=pic)
+    bot.sendSticker(chat_id=update.message.chat_id, sticker=pic);
+proof_handler = CommandHandler('proof', proof)
+dispatcher.add_handler(proof_handler)
 
-@bot.message_handler(content_types=["text"])
-def grustno(update.message):
-    incoming=message.text
-    if incoming=='Грустно':
-        pic=open('s1200.jpeg', 'rb')
-        bot.send_photo(chat_id=update.message.chat_id, photo=pic)
-        bot.sendMessage(chat_id=update.message.chat_id, text="Ни грустииии")
+def grustno(bot, update):
+    pic=open('s1200.jpeg', 'rb')
+    bot.send_photo(chat_id=update.message.chat_id, photo=pic);
+    bot.sendMessage(chat_id=update.message.chat_id, text="Ни грустииии")
+grustno_handler = CommandHandler('grustno', grustno)
+dispatcher.add_handler(grustno_handler)
 
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
-                      url_path=TOKEN)
-updater.bot.set_webhook("https://stresscatbot.herokuapp.com/" + TOKEN)
-updater.idle()
+                      url_path=config.token)
+updater.bot.set_webhook("https://stresscatbot.herokuapp.com/" + config.token)
