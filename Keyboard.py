41bd5503c9a4a13be00e27a38947177cde2079e1
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-23 11:53:19 +0300

    Update Keyboard.py

--- Keyboard.py
+++ Keyboard.py
@@ -6,11 +6,12 @@ from config import token
 bot = telebot.TeleBot(token)
 
 class Keyboard:
-    def start_keyboard(message):
+    def keyboard(message):
         markup = types.ReplyKeyboardMarkup()
-        button1 = types.KeyboardButton('\ud83d\udcbc Simple Keyboard')# Для того что бы к кнопке или сообщению добавить Emoji нужно
-        button2 = types.KeyboardButton('\ud83c\udfe2 Inline Keyboard')#в базе юникода выбрать смайл, взять его индекс, и забить
-        button3 = types.KeyboardButton('\ud83d\udcf2 Callback Inline')#этот индекс на сайт https://www.charbase.com и оттуда взять код из графы JavaEscape. Я делал так.
-        markup.row(button1, button2)#2 кнопки в ряд
-        markup.row(button3)#одна кнопка в ряд. Думаю принцип понятен
+        button1 = types.KeyboardButton('/secret')# Для того что бы к кнопке или сообщению добавить Emoji нужно
+        button2 = types.KeyboardButton('/proof')#в базе юникода выбрать смайл, взять его индекс, и забить
+        button3 = types.KeyboardButton('/grustno')#этот индекс на сайт https://www.charbase.com и оттуда взять код из графы JavaEscape. Я делал так.
+        button4 = types.KeyboardButton('/grustno')
+        markup.row(button1, button2, button3)#2 кнопки в ряд
+        markup.row(button4)#одна кнопка в ряд. Думаю принцип понятен
         bot.send_message(message.chat.id, 'Main menu: ', reply_markup=markup)#Важный момент, если переменная markup которую вы создали и заполнили выше носит другое название, например krakozyabra, то тут нужно будет указать krakozyabra
