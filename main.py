b5ebdc88fd75aaa32bcbb631cb7252b194f62bc4
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-24 13:59:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ updater = Updater(config.token)
 dispatcher = updater.dispatcher
 bot = telebot.TeleBot(config.token)
 
-markup = types.ReplyKeyboardMarkup()
-button1 = types.KeyboardButton('/secret')
-button2 = types.KeyboardButton('/proof')
-button3 = types.KeyboardButton('/grustno')
-button4 = types.KeyboardButton('/help')
+def start(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="Привет, если хочешь узнать тайну, отправь /secret Если нужны доказательства - отправь /proof Если грустно - отправь /grustno", reply_markup=markup)	
+    markup = types.ReplyKeyboardMarkup()
+    button1 = types.KeyboardButton('/secret')
+    button2 = types.KeyboardButton('/proof')
+    button3 = types.KeyboardButton('/grustno')
+    button4 = types.KeyboardButton('/help')
 markup.row(button1, button2, button3)
 markup.row(button4)
 
-def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Привет, если хочешь узнать тайну, отправь /secret Если нужны доказательства - отправь /proof Если грустно - отправь /grustno", reply_markup=markup)	
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
@@ -37,7 +37,7 @@ def help(bot, update):
 help_handler = CommandHandler('help', help)
 dispatcher.add_handler(help_handler)
 
-#stickers for random
+#stickers for randint
 stickers = ["CAADAgADCwADlp-MDpuVH3sws_a7FgQ", "CAADAgAD7g0AAqgILwj_8DhBu2dnDRYE", "CAADBAADfQADzjkIDSgZQLclD7jiFgQ", "CAADBAADRAADzjkIDbv4-ULKD6hiFgQ", "CAADAgAD0gIAArnzlwt4AXAE0tVijhYE", "CAADAgAD2gEAAsdjXBUX3pc5V_GYDBYE", "CAADBAADmAADzjkIDRaa2RCZbCJWFgQ", "CAADBAADkwADzjkIDYydFNXPYxHoFgQ", "CAADAgAD4w0AAqgILwh6UH_uBQWn_RYE", "CAADAgADBAgAAhhC7ghzMDDTpZ3HjRYE", "CAADAgADCAADl_TGFHTucAABYtoR1BYE"]
 
 def proof(bot, update):
