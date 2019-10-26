fa12034ed0bf6c8c64ef0e958987ab3c566f28cd
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-26 11:18:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 TOKEN = '951362784:AAGOdkDDUC5F2K8VqvMS-zvn25XIxjDNzRA'
 
- URLHEROKU = 'https://stresscatbot.herokuapp.com/'
+URLHEROKU = 'https://stresscatbot.herokuapp.com/'
