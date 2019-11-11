0ff0dea7538ed1b3fce02dec51f7612736385185
B00bot <55877415+B00bot@users.noreply.github.com>
2019-11-12 01:15:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def user_comand(msg):
 
 @bot.message_handler(commands=['start'])
 def start_message(msg):
-    newtgid=tgid(msg.user.id)
-    newuser = name(msg.user.name)
+    newtgid= Users.tgid(msg.user.id)
+    newuser = Users.name(msg.user.name)
     session.add(newuser, newtgid)
     bot.send_message(msg.chat.id, """Если хочешь узнать тайну, нажми ❤️ Секрет ❤️ 
 Если нужны доказательства
