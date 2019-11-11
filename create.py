12a584d8ee7ffe02d8b30ca1d1de0ed0e3b97f2f
B00bot <55877415+B00bot@users.noreply.github.com>
2019-11-12 01:07:57 +0300

    Update create.py

--- create.py
+++ create.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, MetaData, Table, create_engine
 from sqlalchemy.orm import mapper, sessionmaker
-from model import emgine, session, base
+from model import engine, session, base
 meta = MetaData()
 Users = Table(
    'users', meta,
