dfa874589c6369ad2ceb862a23e0bf0d1593e146
B00bot <55877415+B00bot@users.noreply.github.com>
2019-11-12 03:26:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import keyboard
 from keyboard import keyb
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
-mapper=
+
 
 
 stickers = ["CAADAgADCwADlp-MDpuVH3sws_a7FgQ", "CAADAgAD7g0AAqgILwj_8DhBu2dnDRYE", "CAADBAADfQADzjkIDSgZQLclD7jiFgQ", "CAADBAADRAADzjkIDbv4-ULKD6hiFgQ", "CAADAgAD0gIAArnzlwt4AXAE0tVijhYE", "CAADAgAD2gEAAsdjXBUX3pc5V_GYDBYE", "CAADBAADmAADzjkIDRaa2RCZbCJWFgQ", "CAADBAADkwADzjkIDYydFNXPYxHoFgQ", "CAADAgAD4w0AAqgILwh6UH_uBQWn_RYE", "CAADAgADBAgAAhhC7ghzMDDTpZ3HjRYE", "CAADAgADCAADl_TGFHTucAABYtoR1BYE"]
