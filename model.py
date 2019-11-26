9578e1a5d9e1a367c0a920d8f0a30c15ef44f252
B00bot <55877415+B00bot@users.noreply.github.com>
2019-11-26 16:11:14 +0300

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import mapper, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("postgres://kdbqihrelkjdyh:5703c7cbdcbb12d999e210368936b8b734940f07d4b6357ff3d5eed99d6e3b91@ec2-54-247-85-251.eu-west-1.compute.amazonaws.com:5432/d6lcrvpadm6o79")
+engine = create_engine("postgres://wzyikgbuzredqk:d378505fe38ff6c1d9adab45e6a05774c20693d524fb8a2a4c13fd6a034fe751@ec2-46-137-120-243.eu-west-1.compute.amazonaws.com:5432/d57dh43iuefimi")
 Session = sessionmaker(bind=engine)
 base = declarative_base(engine)
 session = Session()
